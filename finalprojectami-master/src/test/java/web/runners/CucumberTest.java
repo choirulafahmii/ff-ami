1fe18d83466e58af6321386cca6c18ec8029cb74
choirulafahmi <choirulafahmi.work@gmail.com>
2025-06-17 19:20:54 +0700

    runners

--- finalprojectami-master/src/test/java/web/runners/CucumberTest.java
+++ finalprojectami-master/src/test/java/web/runners/CucumberTest.java
@@ -6,7 +6,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = "src/test/resources/features/web",
+        features = "src/test/resources/Features/Web",
         glue = "web.stepdefinitions",
         plugin = {"pretty", "html:target/cucumber-endtoend-reports.html", "json:target/cucumber-endtoend-reports.json"},
         tags = ""
